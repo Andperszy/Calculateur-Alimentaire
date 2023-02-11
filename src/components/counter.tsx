f5dce7bccb8a212a31dcaff3d81bf7a2bc9bd3ae
Temavers <119498691+Temavers@users.noreply.github.com>
2023-02-11 01:19:07 +0100

    Tronçons de 50 au lieu de 100

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -8,8 +8,8 @@ const Counter: React.FC<CounterProps> = ({ type, count, setCount }) => {
   return (
     <div className="card">
       <p>{type} {count} g/jour</p>
-      <button disabled={count < 1} onClick={() => setCount(count - 100)}>Moins</button>
-      <button onClick={() => setCount(count + 100)}>Plus</button>
+      <button disabled={count < 1} onClick={() => setCount(count - 50)}>Moins</button>
+      <button onClick={() => setCount(count + 50)}>Plus</button>
     </div>
   );
 };
